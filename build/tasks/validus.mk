fd27a10fcc5f9c8895c9324ad563361eedabef6a
theringmaster <nytesblood@gmail.com>
2020-04-05 16:44:56 -0400

    Add build script based on Blissify
    https://github.com/BlissRoms/platform_vendor_bliss/commit/162999fca695f222a20e6cab9b338f46d69e2aa5#diff-83a220a26c2327fc80d88390b9bf0b92

---
+++ build/tasks/validus.mk
@@ -0,0 +1,36 @@
+# Copyright (C) 2016-2018 The BlissOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# validus Target Zip
+VALIDUS_TARGET_PACKAGE := $(PRODUCT_OUT)/$(VALIDUS_VERSION).zip
+
+.PHONY: validus
+validus: $(INTERNAL_OTA_PACKAGE_TARGET)
+	
+	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(VALIDUS_TARGET_PACKAGE)
+	$(hide) $(MD5SUM) $(VALIDUS_TARGET_PACKAGE) > $(VALIDUS_TARGET_PACKAGE).md5
+	@echo -e ${CL_RED}""${CL_RED}
+	@echo -e ${CL_RED}"    ▄█    █▄     ▄████████  ▄█        ▄█  ████████▄  ███    █▄     ▄████████ "${CL_RED}
+	@echo -e ${CL_RED}"   ███    ███   ███    ███ ███       ███  ███   ▀███ ███    ███   ███    ███ "${CL_RED}
+	@echo -e ${CL_RED}"   ███    ███   ███    ███ ███       ███▌ ███    ███ ███    ███   ███    █▀  "${CL_RED}
+	@echo -e ${CL_RED}"   ███    ███   ███    ███ ███       ███▌ ███    ███ ███    ███   ███        "${CL_RED}
+	@echo -e ${CL_RED}"   ███    ███ ▀███████████ ███       ███▌ ███    ███ ███    ███ ▀███████████ "${CL_RED}
+	@echo -e ${CL_RED}"   ███    ███   ███    ███ ███       ███  ███    ███ ███    ███          ███ "${CL_RED}
+	@echo -e ${CL_RED}"   ███    ███   ███    ███ ███▌    ▄ ███  ███   ▄███ ███    ███    ▄█    ███ "${CL_RED}
+	@echo -e ${CL_RED}"    ▀██████▀    ███    █▀  █████▄▄██ █▀   ████████▀  ████████▀   ▄████████▀  "${CL_RED}
+	@echo -e ${CL_RED}"                           ▀                                                 "${CL_RED}
+	@echo -e ${CL_RED}"                           A Ground Zero Roms "${CL_RED}
+	@echo -e ${CL_RED}"                                Project      "${CL_RED}
+	@echo -e "Package complete: $(VALIDUS_TARGET_PACKAGE)"
+
